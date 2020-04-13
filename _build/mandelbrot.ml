951061dd8548fccaad310a9e4d12c6f12d4338f8
xopheS <xophesaad@gmail.com>
2020-04-13 08:00:36 +0200

    Add cmd args but syntax error

--- _build/mandelbrot.ml
+++ _build/mandelbrot.ml
@@ -5,6 +5,15 @@ module Example (K : Kahn.S) = struct
 	module K = K
 	module Lib = Kahn.Lib(K)
 	open Lib
+	
+	(** AUXILIARY FUNCTIONS **)
+	(** Range function **)
+	let (--) (first : int) (last : int) = 
+    		let rec aux n acc =
+      			if n < first then acc else aux (n - 1) (n :: acc)
+    		in 
+		aux last []
+
 
 	(** TYPE COMPLEX WITH SOME OPERATIONS **)
 	type complex = float * float
@@ -18,10 +27,29 @@ module Example (K : Kahn.S) = struct
 	(** GLOBAL VARIABLES **)
 	let width : int ref = ref 1300
 	let height : int ref = ref 1000
-	let n_iter : int ref = ref 255 
-	let origin : complex ref = ref (-0.5 , 0.)
-	let zm : float ref = ref 2.	
+	let n_iter : int ref = ref 255
+	let np : int ref = ref 1
+
+	let xo : float ref = ref (-0.5)
+	let yo : float ref = ref 0.
 	
+	let zm : float ref = ref 2.
+	let w : int ref = ref 1300
+	
+	let user_inputs = [("-w", Arg.Set_int width, "Width");
+			   ("-h", Arg.Set_int height, "Height");
+			   ("-p", Arg.Set_int np, "Number of processes (must divide width)");
+			   ("-n", Arg.Set_int n_iter, "Number of iterations");
+			   ("-xo", Arg.Set_float xo, "Real value of origin");
+			   ("-yo", Arg.Set_float yo, "Imaginary value of origin");
+			   ("-z", Arg.Set_float zm, "Scale value, half length of axis");]	
+		
+	exception Invalid_Argument of string
+
+	let require (predicate : bool) (msg : string) : unit =
+		if not predicate then
+			raise (Invalid_Argument msg)
+
 	type point = { x : int; y : int; c : color }
 		
 	(** MANDELBROT SEQUENCE STEP **)
@@ -42,11 +70,11 @@ module Example (K : Kahn.S) = struct
 		
 		(** center around (x_st, y_st) and normalize according to zoom scale **)
 
-		let x_cart = center (float_of_int x) (float_of_int !width) (re !origin) in
-		let r = normalize x_cart ((re !origin) -. !zm) ((re !origin) +. !zm) (float_of_int !width) in
+		let x_cart = center (float_of_int x) (float_of_int !width) !xo in
+		let r = normalize x_cart (!xo -. !zm) (!xo +. !zm) (float_of_int !width) in
 
-		let y_cart = center (float_of_int y) (float_of_int !height) (im !origin) in
-		let i = normalize y_cart ((im !origin) -. !zm) ((im !origin) +. !zm) (float_of_int !height) in
+		let y_cart = center (float_of_int y) (float_of_int !height) !yo in
+		let i = normalize y_cart (!yo -. !zm) (!yo +. !zm) (float_of_int !height) in
 
 		r , i
 
@@ -86,20 +114,19 @@ module Example (K : Kahn.S) = struct
 		{ x = x; y = y; c = new_color }
 	
 	
-	let eval_canvas ((x_st, y_st) : int * int) (cw : int) (ch : int) (qo : point K.out_port) : unit K.process =
+	let eval_canvas (x_st : int) (qo : point K.out_port) : unit K.process =
 		
-		let max_x : int = x_st + cw in
-		let max_y : int = y_st + ch in
+		let max_x : int = x_st + !w in
 				
 		let rec compute ((x, y) : int * int) : unit K.process =
 			(K.put (eval_point (x, y)) qo) >>=
 				(fun () ->
 					if x < max_x then compute (x + 1, y)
-					else if y < max_y then compute (x_st, y + 1) 
+					else if y < !height then compute (x_st, y + 1) 
 					else K.return ()
 				)
 		in	
-		compute (x_st, y_st)
+		compute (x_st, 0)
 		
 	
 	let plot_all (qi : point K.in_port) : unit K.process =
@@ -108,16 +135,32 @@ module Example (K : Kahn.S) = struct
 					(set_color (p.c); plot p.x p.y; lget ()))
 		in
 		lget ()	
+	
+	
+	let configurations () : unit =
+		(** parse command line arguments **)
+		Arg.parse (Arg.align user_inputs) (fun _ -> ()) "";
+		
+		(** check validity **)
+		require (!width mod !np = 0) "Number of processes must divide width";
+		require (!zm > 0.) "Zoom must have a positive value";
+		w := !width / !np;
+		
+		(** open graph **)
+		open_graph (
+			String.concat "" [" "; (string_of_int !width); "x"; (string_of_int !height)]
+		);
+		set_window_title "Mandelbrot";
+		
 
 
 	let main : unit K.process = 
-		open_graph " 1300x1000";
-		set_window_title "Mandelbrot";
-		
+		configurations ();
+
 		(delay K.new_channel ()) >>=
-		(fun (qi, qo) -> K.doco [eval_canvas (650, 0) 650 1000 qo;
-					eval_canvas (0, 0) 650 1000 qo;
-					 plot_all qi; ])
+		(fun (qi, qo) -> K.doco (
+		List.append (List.map (fun i -> eval_canvas (i * !w) qo) (0--(!np-1)))
+			    [plot_all qi]))
 
 
 end
@@ -126,7 +169,7 @@ end
 
 
 
-module E = Example(Sequential.Seq)
+module E = Example(Kahn.Th)
 
 let () = E.K.run E.main
 
