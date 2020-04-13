d03d225ffee5f65eebbe88cfef9523a7c96d8f22
xopheS <xophesaad@gmail.com>
2020-04-13 11:03:08 +0200

    All functional

--- mandelbrot.ml
+++ mandelbrot.ml
@@ -14,7 +14,9 @@ module Example (K : Kahn.S) = struct
     		in 
 		aux last []
 
-
+	(** POINT STRUCTURE (COORDINATES AND COLOR) **)
+	type point = { x : int; y : int; c : color }
+	
 	(** TYPE COMPLEX WITH SOME OPERATIONS **)
 	type complex = float * float
 	
@@ -22,7 +24,6 @@ module Example (K : Kahn.S) = struct
 	let im ((_ , y) : complex) : float = y
 	
 	let squared_modulus (z : complex) : float = re z ** 2. +. im z ** 2.
-	
 	
 	(** GLOBAL VARIABLES **)
 	let width : int ref = ref 1300
@@ -35,22 +36,35 @@ module Example (K : Kahn.S) = struct
 	
 	let zm : float ref = ref 2.
 	let w : int ref = ref 1300
+	let r : float ref = ref 4. (** escape radius squared **)
 	
+
+	(** COMMAND LINE INPUT HANDLING **)
 	let user_inputs = [("-w", Arg.Set_int width, "Width");
 			   ("-h", Arg.Set_int height, "Height");
 			   ("-p", Arg.Set_int np, "Number of processes (must divide width)");
 			   ("-n", Arg.Set_int n_iter, "Number of iterations");
 			   ("-xo", Arg.Set_float xo, "Real value of origin");
 			   ("-yo", Arg.Set_float yo, "Imaginary value of origin");
-			   ("-z", Arg.Set_float zm, "Scale value, half length of axis");]	
-		
+			   ("-z", Arg.Set_float zm, "Scale value, half length of axis");	
+			   ("-r", Arg.Set_float r, "Squared escape radius");]	
+	
+	
 	exception Invalid_Argument of string
 
 	let require (predicate : bool) (msg : string) : unit =
 		if not predicate then
 			raise (Invalid_Argument msg)
 
-	type point = { x : int; y : int; c : color }
+	let parse () : unit =
+		(** parse command line arguments **)
+		Arg.parse (Arg.align user_inputs) (fun _ -> ()) "";
+		
+		(** check validity **)
+		require (!width mod !np == 0) "Number of processes must divide width";
+		require (!zm > 0.) "Zoom must have a positive value";
+		w := !width / !np
+	
 		
 	(** MANDELBROT SEQUENCE STEP **)
 	let step (z : complex) (c : complex) : complex =
@@ -69,7 +83,6 @@ module Example (K : Kahn.S) = struct
 	let scale ((x, y) : int * int) : complex =
 		
 		(** center around (x_st, y_st) and normalize according to zoom scale **)
-
 		let x_cart = center (float_of_int x) (float_of_int !width) !xo in
 		let r = normalize x_cart (!xo -. !zm) (!xo +. !zm) (float_of_int !width) in
 
@@ -79,41 +92,38 @@ module Example (K : Kahn.S) = struct
 		r , i
 
 
-	(** COLOR FUNCTION OF ITERATION **)	
-	let red (v : int) : int =
-		(**if v = n_iter then 0 else 255**)
-		v
-	
-	let green (v : int ) : int = 
-		(**if v = n_iter then 0 else 255**)
-		v
+	(** COLOR FUNCTION OF ITERATION AND VALUE REACHED **)	
+	let find_color (v : int) (z : complex) : color =
+		let p = log (max (squared_modulus z) 1.) /. (2. ** (float_of_int v)) in
+		let logp = log p /. log 2. in
+		
+		let a = (1. /. log 2.) in
+		let b = 1. /. (log 2. *. 3. *. 2. ** 0.5) in
+		let c = 1. /. (log 2. *. 7. *. 3. ** (1. /. 8.)) in
+		let design = fun x -> int_of_float ((1. -. cos (x *. logp)) /. 2. *. 255.) in
+		
+		rgb (design a) (design b) (design c)
 
-	let blue (v : int) : int =	
-		(**if v = n_iter then 0 else 255**)
-		v
-	
-
-	
+		
+	(** POINT EVALUATION FUNCTION **)
 	let eval_point ((x, y) : int * int) : point = 
 		
 		let c : complex = scale (x, y) in
 	
-		let rec eval_rec (z : complex) (iter : int) : int = 
+		let rec eval_rec (z : complex) (iter : int) : int * complex = 
 
-			if squared_modulus z > 4. || iter = !n_iter then
-				iter
-
+			if squared_modulus z > !r || iter = !n_iter then
+				iter, z  
 			else
 				eval_rec (step z c) (iter + 1)
 		in
 		
-		let i = eval_rec (0. , 0.) 0 in
+		let i, z = eval_rec (0. , 0.) 0 in
 		
-		(** Format.printf "(%f , %f, %i)\n" (re c) (im c) i; **)
-		let new_color = rgb (red i) (green i) (blue i) in
-		{ x = x; y = y; c = new_color }
+		{ x = x; y = y; c = find_color i z}
 	
 	
+	(** CANVAS EVALUATION **)	
 	let eval_canvas (x_st : int) (qo : point K.out_port) : unit K.process =
 		
 		let max_x : int = x_st + !w in
@@ -128,7 +138,7 @@ module Example (K : Kahn.S) = struct
 		in	
 		compute (x_st, 0)
 		
-	
+	(** PLOT PROCESS **)
 	let plot_all (qi : point K.in_port) : unit K.process =
 		let rec lget () =
 			(K.get qi) >>= (fun (p : point) -> 
@@ -137,16 +147,9 @@ module Example (K : Kahn.S) = struct
 		lget ()	
 	
 	
-	let configurations () : unit =
-		(** parse command line arguments **)
-		Arg.parse (Arg.align user_inputs) (fun _ -> ()) "";
-		
-		(** check validity **)
-		require (!width mod !np == 0) "Number of processes must divide width";
-		require (!zm > 0.) "Zoom must have a positive value";
-		w := !width / !np;
-		
-		(** open graph **)
+	
+	(** GRAPH INIT **)
+	let set_graph () : unit =
 		open_graph (
 			String.concat "" [" "; (string_of_int !width); "x"; (string_of_int !height)]
 		);
@@ -155,9 +158,9 @@ module Example (K : Kahn.S) = struct
 
 
 	let main : unit K.process = 
-		configurations ();
-
-		(delay K.new_channel ()) >>=
+		(delay parse ()) >>=
+		(fun () -> delay set_graph ()) >>=
+		(fun () -> delay K.new_channel ()) >>=
 		(fun (qi, qo) -> K.doco (
 		List.append (List.map (fun i -> eval_canvas (i * !w) qo) (0--(!np-1)))
 			    [plot_all qi]))
